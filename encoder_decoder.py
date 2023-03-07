7a51cf0be957ce3723bcd4086bb917dd9c6f8490
Ashley <ashleyemaurer@gmail.com>
2023-03-07 15:31:40 -0500

    changed encoder_decoder to only encode a password

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -11,12 +11,3 @@ if __name__ == "__main__":
             orig_pass = input("Please enter your password to encode: ")
             encoded_pass = [int(i) + 3 for i in orig_pass]
             print("Your password has been encoded and stored!\n")
-        elif option == 2:
-            decoded_pass = [int(i) - 3 for i in encoded_pass]
-            print("The encoded password is ", end= " ")
-            for i in encoded_pass:
-                print(f"{i}", end="")
-            print(", and the original password is", end=" ")
-            for i in decoded_pass:
-                print(f"{i}", end="")
-            print(".\n")
